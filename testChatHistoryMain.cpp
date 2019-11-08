926064c2bda8899dd5eeb205b35a44cfe2eece92
janna <myemail@mail>
2019-11-08 03:29:56 +0000

    chat history objects stored in database, chathistory, chatmanager and fileinfo completed and tested

---
+++ testChatHistoryMain.cpp
@@ -0,0 +1,22 @@
+#include "ChatHistory.h"
+#include "ChatManager.h"
+
+
+int main(){
+ChatManager myManager;
+
+myManager.createChat("frank","private");
+myManager.createChat("Heather", "private");
+myManager.createChat("Steven","private");
+
+cout<<"testing to see if chat has been added to database"<<endl;
+myManager.printDatabaseContents();
+
+cout<<"testing to see if add access works"<<endl;
+myManager.addAccess("1", "Heather");
+myManager.printDatabaseContents();
+cout<<"Testin to see if add Message works"<<endl;
+cout<<myManager.addMessage("1", "frank", "yo whats up heather")<<"\n\n";
+cout<<myManager.addMessage("1","Heather","not much, u?")<<"\n\n";
+cout<<myManager.retrieveChat("1","frank","private");
+}
